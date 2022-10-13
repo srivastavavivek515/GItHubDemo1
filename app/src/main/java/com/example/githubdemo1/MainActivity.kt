1d6a8b10268c748c7a2f89c06a5b8a8ca19762cf
Vivek Srivastav <svivek@altametrics.com>
2022-10-13 19:51:50 +0530

    Log.d("company _one","This is the first  commit")

--- app/src/main/java/com/example/githubdemo1/MainActivity.kt
+++ app/src/main/java/com/example/githubdemo1/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("company _one","This is the first  commit")
     }
 }
